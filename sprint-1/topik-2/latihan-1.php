e34716e3872109bb5737cf71298720a64b92bbae
bagussona <bagussona87@gmail.com>
2021-01-26 06:38:25 +0700

    upload tugas 2.1 soal nomer 1 & 2

---
+++ sprint-1/topik-2/latihan-1.php
@@ -0,0 +1,15 @@
+<?php
+$p = 5;
+$l = 8;
+$hasil = $p*$l;
+
+echo "Dik: Panjang seuah persegi Panjang adalah $p<br>";
+echo "Lebar sebuah persegi Panjang adalah $l<br><br>";
+echo "Dit: Berapakah hasil dari panjang x lebar?<br><br>";
+echo "Jawab :<br><br>";
+echo "P x L =<br>";
+echo ("$p x $l = <br>");
+//echo ($p * $l);
+echo $hasil;
+
+?>
---
+++ sprint-1/topik-2/latihan-2.php
@@ -0,0 +1,24 @@
+<?php
+$d = 9;
+$r = ($d / 2);
+$phi = (22/7);
+
+
+echo "Luas lingkaran adalah = phi * r2<br>";
+echo "dik: diameter lngkaran = $d <br>";
+echo "jari-jari lingkaran $d / 2 = $r <br>";
+echo "<br>";
+echo ($phi * $r * $r);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Keliling lingkaran adalah = 2*phi*r<br>";
+echo "dik: diameter lngkaran = $d <br>";
+echo "jari-jari lingkaran $d / 2 = $r <br>";
+echo "<br>";
+echo (2 * $phi * $r);
+
+
+
+?>
