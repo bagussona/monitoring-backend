ddee7e6fdffe758e878bab866e1dbbd33d0f1825
bagussona <bagussona87@gmail.com>
2021-01-26 10:49:52 +0700

    edit, delete, dan rename tugas

--- sprint-1/topik-2/latihan-4.php
+++ sprint-1/topik-2/latihan-4.php
@@ -1,3 +1,15 @@
-<!-- <?php
+<?php 
+$n = 5;
+$n1 = 9;
+$n2 = 19;
 
- ?> -->
+$r = $n*2+2;
+$r1 = $n1*2+2;
+$r2 = $n2*2+2;
+
+echo "rumus yang dipakai, (n*2+2) \n";
+echo "1. jika n=$n, nilai r: $r \n";
+echo "2. jika n=$n1, nilai r: $r1 \n";
+echo "3. jika n=$n2, nilai r: $r2\n";
+
+?>
