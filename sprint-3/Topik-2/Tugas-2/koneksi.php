993c41e9f36679d58c6e0143f91bc967b8b1611a
bagussona <bagussona87@gmail.com>
2021-02-13 14:49:22 +0700

    salah aca soal kwkw

---
+++ sprint-3/Topik-2/Tugas-2/koneksi.php
@@ -0,0 +1,38 @@
+<?php
+// $servername = "localhost";
+// $username = "bagus";
+// $password = "awsd123";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, "lights_life_company");
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// echo "Connected successfully";
+
+
+
+// ##########################
+
+$servername = "localhost";
+$username = "bagus";
+$password = "awsd123";
+$dbname = "lights_life_company";
+
+try {
+    $conn = new PDO("mysql:host=$servername;$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+
+
+?>
